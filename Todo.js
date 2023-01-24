c7b5589078ff31e7ea93f2c71fbb6dfd4bae614e
Tiago Fassoni <tiagofassoni@protonmail.com>
2023-01-24 19:14:25 -0300

    Adds a function to read from local storage
    *This is still a draft*
    
    - Added nanoid to the global object, to "escape" the module isolation.
    - Created a function that reads from local storage.
    - TODO: function needs to be called on initialization
    - TODO:  remove the "delete" bug

--- Todo.js
+++ Todo.js
@@ -9,21 +9,43 @@ const saveToLocalStorage = () => {
       const item = list.children[i];
       const isChecked = item.querySelector('input[type=checkbox]').checked;
       const text = item.textContent;
-      items.push({ isChecked, text });
+      const nanoid = item.nanoid;
+      items.push({ isChecked, nanoid, text });
     }
     localStorage.setItem('items', JSON.stringify(items));
   };
 
+const readFromLocalStorage = () => {
+  let items = localStorage.getItem('items')
+  if (items.length != 0) {
+     items = JSON.parse(items)
+  }
+  
+  for(let item of items) {
+    let newItem = document.createElement('li')
+    newItem.innerHTML = `
+
+    <input type="checkbox" />
+    ${item.text}
+    <button class="delete">Delete</button>
+  `;
+    newItem.setAttribute("nanoid", item.nanoid);
+    list.appendChild(newItem);
+    console.log('done')
+
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const newItem = document.createElement('li');
-  newItem.setAttribute("id", uuidv4())
   newItem.innerHTML = `
 
     <input type="checkbox">
     ${itemInput.value}
     <button class="delete">Delete</button>
   `;
+  newItem.setAttribute("nanoid", window.nanoid());
   list.appendChild(newItem);  // append the new list item to the unordered list
   itemInput.value = '';
   saveToLocalStorage();
